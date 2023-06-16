17ee7e0ea881a8b516dbb648424a621c10f1e3df
Abu Sayed <rafsansayed132@gmail.com>
2023-06-16 14:29:15 +0600

    update login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 
 const Login = () => {
-    
+
 
     return (
         <div className='h-[800px] flex justify-center items-center'>
@@ -11,26 +10,22 @@ const Login = () => {
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Email</span></label>
                         <input type="text"
-                            
+
                             className="input input-bordered w-full max-w-xs" />
-                        
+
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Password</span></label>
-                        <input type="password"
-                           
-                            className="input input-bordered w-full max-w-xs" />
-                        <label className="label"> <span className="label-text">Forget Password?</span></label>
-                        
+                        <input type="password" className="input input-bordered w-full max-w-xs" />
                     </div>
                     <input className='btn btn-accent w-full' value="Login" type="submit" />
                     <div>
-                       
+
                     </div>
                 </form>
-                <p className="mt-2">dont have account?<Link className='text-secondary' to="/signup">Create new Account</Link></p>
-                
-                
+
+
+
             </div>
         </div>
     );
